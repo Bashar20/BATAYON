98e0ae207416e9750ccb74262201a70fdb467b15
bashar <khairulislam0292020@gmail.com>
2022-09-08 14:16:47 +0600

    first commit

---
+++ english/accounts/urls.py
@@ -0,0 +1,23 @@
+from re import template
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from accounts import views
+from accounts.views import RegistrationView, ProfileView
+
+urlpatterns = [
+    path('',views.index,name='home'),
+    path('register/', RegistrationView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
+]
